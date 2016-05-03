c677415529298c3005d0f12f3dba3d1a1b5b6ea8
koczurekk <koczurekk@gmail.com>
2016-05-03 06:37:41 +0200

    Added QResourceStream class

---
+++ QSFML/qresourcestream.cpp
@@ -0,0 +1,59 @@
+#include "qresourcestream.hpp"
+
+#include <iostream>
+namespace qsf
+{
+    QResourceStream::QResourceStream()
+    {
+        this->pos = 0;
+        this->size = 0;
+    }
+    QResourceStream::~QResourceStream()
+    {
+        if(size != 0)
+            free(dat);
+    }
+
+    bool QResourceStream::open(const char* path)
+    {
+        QFile file(path);
+        if(!file.open(QIODevice::ReadOnly))
+            return false;
+
+        this->size = file.size();
+        dat = malloc(file.size());
+
+        file.read((char*) dat, file.size() + 1);
+        file.close();
+
+        return true;
+    }
+
+    sf::Int64 QResourceStream::read(void *data, sf::Int64 size)
+    {
+        sf::Int64 i;
+        for(i = 0; i < size && i + pos < this->size; i++, pos++)
+            ((char*)data)[i] = ((char*)dat)[pos];
+
+        return i;
+    }
+    sf::Int64 QResourceStream::seek(sf::Int64 position)
+    {
+        pos = position;
+        return tell();
+    }
+    sf::Int64 QResourceStream::tell()
+    {
+        return pos;
+    }
+    sf::Int64 QResourceStream::getSize()
+    {
+        return this->size;
+    }
+
+    QResourceStream & QResourceStream::operator()(const char* path)
+    {
+        open(path);
+        return *this;
+    }
+}
