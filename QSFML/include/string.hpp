54f2d1dcdb606fa34adfb0c28491a703b0f2171a
koczurekk <koczurekk@gmail.com>
2016-06-11 23:34:56 +0200

    Short license / author notes in string.* files

--- QSFML/include/string.hpp
+++ QSFML/include/string.hpp
@@ -1,3 +1,8 @@
+/* =========================================================== *
+ * QSFML (c) Kamil Koczurek | koczurekk@gmail.com	       *
+ * GNU GPL v3 License http://www.gnu.org/licenses/gpl-3.0.html *
+ * =========================================================== */
+
 #ifndef STRING_H
 #define STRING_H
 
