bd00a08c1d632f17bbd38d9ef8bf54dbaeffc49b
koczurekk <koczurekk@gmail.com>
2016-11-04 16:16:14 +0100

    Added resize event support

--- QSFML/include/QSFMLCanvas.hpp
+++ QSFML/include/QSFMLCanvas.hpp
@@ -12,54 +12,58 @@
 #include <QMouseEvent>
 #include <QWheelEvent>
 #include <QKeyEvent>
+#include <qdebug.h>
 #include <QWidget>
 #include <QTimer>
 
 namespace qsf
 {
-    class QSFMLWidget
-        : public QWidget,
-          public sf::RenderWindow
-    {
-    public:
-        explicit QSFMLWidget(QWidget *parent, const QPoint& Position, const QSize& Size, unsigned int FrameTime = 7); //Defauld frame time should give about 144 FPS
-        virtual ~QSFMLWidget();
+	class QSFMLWidget
+	: public QWidget,
+	public sf::RenderWindow
+	{
+	public:
+		explicit QSFMLWidget(QWidget *parent, const QPoint& Position, const QSize& Size, unsigned int FrameTime = 7); //Defauld frame time should give about 144 FPS
+		virtual ~QSFMLWidget();
 
-        virtual void showEvent(QShowEvent*);
-        virtual QPaintEngine* paintEngine() const;
-        virtual void paintEvent(QPaintEvent*);
+		virtual void showEvent(QShowEvent*);
+		virtual QPaintEngine* paintEngine() const;
+		virtual void paintEvent(QPaintEvent*);
 
-        virtual void OnInit();
-        virtual void OnUpdate();
+		virtual void OnInit();
+		virtual void OnUpdate();
 
-        bool pollEvent(sf::Event & ev);
+		bool pollEvent(sf::Event & ev);
 
-        /* =============== *
-         * EVENTS HANDLING *
-         * =============== */
-        //Mouse buttons
-        void mousePressEvent(QMouseEvent *e);
-        void mouseReleaseEvent(QMouseEvent *e);
+		/* =============== *
+		 * EVENTS HANDLING *
+		 * =============== */
+		//Mouse buttons
+		void mousePressEvent(QMouseEvent *e);
+		void mouseReleaseEvent(QMouseEvent *e);
 
-        //Mouse wheel
-        void wheelEvent(QWheelEvent *e);
+		//Mouse wheel
+		void wheelEvent(QWheelEvent *e);
 
-        //Keyboard
-        void keyPressEvent(QKeyEvent *e);
-        void keyReleaseEvent(QKeyEvent *e);
+		//Keyboard
+		void keyPressEvent(QKeyEvent *e);
+		void keyReleaseEvent(QKeyEvent *e);
 
-    private:
-        bool _initialized;
-        QTimer refreshTimer;
-        sf::Time _frameTime;
-        sf::Clock frameClock;
+		//Resize event
+		void resizeEvent(QResizeEvent *event);
 
-    protected:
-        const bool & initialized = _initialized;
-        const sf::Time & frameTime = _frameTime;
+	private:
+		bool _initialized;
+		QTimer refreshTimer;
+		sf::Time _frameTime;
+		sf::Clock frameClock;
 
-        void pushEvent(sf::Event & ev);
-        std::vector<sf::Event> SfEvents;
-    };
+	protected:
+		const bool & initialized = _initialized;
+		const sf::Time & frameTime = _frameTime;
+
+		void pushEvent(sf::Event & ev);
+		std::vector<sf::Event> SfEvents;
+	};
 }
 #endif // QSMLCANVAS_H
--- QSFML/src/QSFMLCanvasEventHandler.cpp
+++ QSFML/src/QSFMLCanvasEventHandler.cpp
@@ -9,59 +9,63 @@
 
 namespace qsf
 {
-    //Mouse buttons events
-    void QSFMLWidget::mousePressEvent(QMouseEvent *e)
-    {
-        sf::Event ev;
-        ev.type = sf::Event::MouseButtonPressed;
+	//Mouse buttons events
+	void QSFMLWidget::mousePressEvent(QMouseEvent *e) {
+		sf::Event ev;
+		ev.type = sf::Event::MouseButtonPressed;
 
-        ev.mouseButton.button = (e->buttons() == Qt::RightButton ? sf::Mouse::Right : sf::Mouse::Left);
-        ev.mouseButton.x = e->localPos().x();
-        ev.mouseButton.y = e->localPos().y();
+		ev.mouseButton.button = (e->buttons() == Qt::RightButton ? sf::Mouse::Right : sf::Mouse::Left);
+		ev.mouseButton.x = e->localPos().x();
+		ev.mouseButton.y = e->localPos().y();
 
-        pushEvent(ev);
-    }
-    void QSFMLWidget::mouseReleaseEvent(QMouseEvent *e)
-    {
-        sf::Event ev;
-        ev.type = sf::Event::MouseButtonReleased;
+		pushEvent(ev);
+	}
+	void QSFMLWidget::mouseReleaseEvent(QMouseEvent *e) {
+		sf::Event ev;
+		ev.type = sf::Event::MouseButtonReleased;
 
-        ev.mouseButton.button = (e->buttons() == Qt::RightButton ? sf::Mouse::Right : sf::Mouse::Left);
-        ev.mouseButton.x = e->localPos().x();
-        ev.mouseButton.y = e->localPos().y();
+		ev.mouseButton.button = (e->buttons() == Qt::RightButton ? sf::Mouse::Right : sf::Mouse::Left);
+		ev.mouseButton.x = e->localPos().x();
+		ev.mouseButton.y = e->localPos().y();
 
-        pushEvent(ev);
-    }
+		pushEvent(ev);
+	}
 
-    //Mouse Wheel events
-    void QSFMLWidget::wheelEvent(QWheelEvent *e)
-    {
-        sf::Event ev;
-        ev.type = sf::Event::MouseWheelMoved;
+	//Mouse Wheel event
+	void QSFMLWidget::wheelEvent(QWheelEvent *e) {
+		sf::Event ev;
+		ev.type = sf::Event::MouseWheelMoved;
 
-        ev.mouseWheel.delta = e->delta() / 8 / 15;
-        ev.mouseWheel.x = sf::Mouse::getPosition(*this).x;
-        ev.mouseWheel.y = sf::Mouse::getPosition(*this).y;
+		ev.mouseWheel.delta = e->delta() / 8 / 15;
+		ev.mouseWheel.x = sf::Mouse::getPosition(*this).x;
+		ev.mouseWheel.y = sf::Mouse::getPosition(*this).y;
 
-        pushEvent(ev);
-        e->accept();
-    }
+		pushEvent(ev);
+		e->accept();
+	}
 
-    //Keyboard events
-    void QSFMLWidget::keyPressEvent(QKeyEvent *event)
-    {
-        sf::Event ev;
-        ev.type = sf::Event::KeyPressed;
-        ev.key.code = QtKeyToSFML(event->key());
+	//Keyboard events
+	void QSFMLWidget::keyPressEvent(QKeyEvent *event) {
+		sf::Event ev;
+		ev.type = sf::Event::KeyPressed;
+		ev.key.code = QtKeyToSFML(event->key());
 
-        pushEvent(ev);
-    }
-    void QSFMLWidget::keyReleaseEvent(QKeyEvent *event)
-    {
-        sf::Event ev;
-        ev.type = sf::Event::KeyReleased;
-        ev.key.code = QtKeyToSFML(event->key());
+		pushEvent(ev);
+	}
+	void QSFMLWidget::keyReleaseEvent(QKeyEvent *event) {
+		sf::Event ev;
+		ev.type = sf::Event::KeyReleased;
+		ev.key.code = QtKeyToSFML(event->key());
 
-        pushEvent(ev);
-    }
+		pushEvent(ev);
+	}
+
+	//Resize event
+	void QSFMLWidget::resizeEvent(QResizeEvent *event) {
+		sf::Event ev;
+		ev.type = sf::Event::Resized;
+		ev.size = { event->size().width(), event->size().height() };
+
+		pushEvent(ev);
+	}
 }
